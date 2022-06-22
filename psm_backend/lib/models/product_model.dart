0acd91b6bd764fc66ac5986f8a70d71120e729bb
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-06-23 02:21:30 +0700

    Added product model

--- psm_backend/lib/models/product_model.dart
+++ psm_backend/lib/models/product_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 
 class Product extends Equatable {
@@ -19,10 +21,81 @@ class Product extends Equatable {
     required this.imageUrl,
     required this.isRecommended,
     required this.isPopular,
-    required this.price,
-    required this.quantity,
+    this.price = 0,
+    this.quantity = 0,
   });
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props {
+    return [
+      id,
+      name,
+      category,
+      description,
+      imageUrl,
+      isRecommended,
+      isPopular,
+      price,
+      quantity,
+    ];
+  }
+
+  Product copyWith({
+    int? id,
+    String? name,
+    String? category,
+    String? description,
+    String? imageUrl,
+    bool? isRecommended,
+    bool? isPopular,
+    int? price,
+    int? quantity,
+  }) {
+    return Product(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        category: category ?? this.category,
+        description: description ?? this.description,
+        imageUrl: imageUrl ?? this.imageUrl,
+        isRecommended: isRecommended ?? this.isRecommended,
+        isPopular: isPopular ?? this.isPopular,
+        price: price ?? this.price,
+        quantity: quantity ?? this.quantity);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'category': category,
+      'description': description,
+      'imageUrl': imageUrl,
+      'isRecommended': isRecommended,
+      'isPopular': isPopular,
+      'price': price,
+      'quantity': quantity
+    };
+  }
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      id: map['id'],
+      name: map['name'],
+      category: map['category'],
+      description: map['description'],
+      imageUrl: map['imageUrl'],
+      isRecommended: map['isRecommended'],
+      isPopular: map['isPopular'],
+      price: map['price'],
+      quantity: map['quantity'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Product.fromJson(String source) =>
+      Product.fromMap(json.decode(source));
+
+  @override
+  bool get stringify => true;
 }
