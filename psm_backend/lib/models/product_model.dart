51c447cb3eedc4c10fd53f6b35be3e342a90e0ae
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-06-23 02:07:52 +0700

    Added product model constructor

--- psm_backend/lib/models/product_model.dart
+++ psm_backend/lib/models/product_model.dart
@@ -1,6 +1,28 @@
 import 'package:equatable/equatable.dart';
 
 class Product extends Equatable {
+  final int id;
+  final String name;
+  final String category;
+  final String description;
+  final String imageUrl;
+  final bool isRecommended;
+  final bool isPopular;
+  int price;
+  int quantity;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.description,
+    required this.imageUrl,
+    required this.isRecommended,
+    required this.isPopular,
+    required this.price,
+    required this.quantity,
+  });
+
   @override
   List<Object?> get props => [];
 }
