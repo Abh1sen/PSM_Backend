87eb580c1783c079bfffa7fcfb8d771759e95dda
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-06-24 22:05:29 +0700

    Product controller added

--- psm_backend/lib/controllers/product_controller.dart
+++ psm_backend/lib/controllers/product_controller.dart
@@ -1,3 +1,24 @@
 import 'package:get/get.dart';
+import 'package:psm_backend/models/models.dart';
 
-class ProductController extends GetxController{}
+class ProductController extends GetxController {
+  List<Product> products = Product.products.obs;
+
+  void updateProductPrice(
+    int index,
+    Product product,
+    double value,
+  ) {
+    product.price = value.toInt();
+    products[index] = product;
+  }
+
+  void updateProductQuantity(
+    int index,
+    Product product,
+    double value,
+  ) {
+    product.quantity = value.toInt();
+    products[index] = product;
+  }
+}
