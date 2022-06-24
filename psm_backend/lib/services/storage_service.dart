d858e9fba8898a0c57fbf8a75a2bfd560b1d9baa
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-06-25 02:12:59 +0700

    minor fixes for storage service

--- psm_backend/lib/services/storage_service.dart
+++ psm_backend/lib/services/storage_service.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:image_picker/image_picker.dart';
 
 class StorageService {
@@ -13,7 +13,7 @@ class StorageService {
 
   Future<String> getDownloadURL(String imageName) async {
     String downloadURL =
-        await storage.ref('product_images'/ $imageName).getDownloadURL();
+        await storage.ref('product_images / $imageName').getDownloadURL();
     return downloadURL;
   }
 }
