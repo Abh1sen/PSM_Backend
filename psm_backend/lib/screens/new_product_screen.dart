040464ddcd53f4f4a085159c52e4ee0a30384532
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-06-25 00:35:24 +0700

    Added form fields and slider for new product

--- psm_backend/lib/screens/new_product_screen.dart
+++ psm_backend/lib/screens/new_product_screen.dart
@@ -25,9 +25,93 @@ class NewProductScreen extends StatelessWidget {
         padding: const EdgeInsets.all(10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [],
+          children: [
+            SizedBox(
+              height: 100,
+              child: Card(
+                margin: EdgeInsets.zero,
+                color: Colors.black,
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Get.to(() => const NewProductScreen());
+                      },
+                      icon: Icon(
+                        Icons.add_circle,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      'Add an Image',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Product Information',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            _buildTextFormField('Product ID'),
+            _buildTextFormField('Product Name'),
+            _buildTextFormField('Product Description'),
+            _buildTextFormField('Product Category'),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildSlider('Price'),
+            _buildSlider('Quantity'),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Row _buildSlider(String title) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 75,
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Slider(
+            value: 0,
+            min: 0,
+            max: 200000,
+            divisions: 20,
+            activeColor: Colors.black,
+            inactiveColor: Colors.black12,
+            onChanged: (value) {},
+          ),
+        ),
+      ],
+    );
+  }
+
+  TextFormField _buildTextFormField(String hintText) {
+    return TextFormField(
+      decoration: InputDecoration(hintText: hintText),
     );
   }
 }
